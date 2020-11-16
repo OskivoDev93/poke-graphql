0cf66a008c3f22722ce9e632bb8557e5ccdd4e15
OskivoDev93 <49086138+OskivoDev93@users.noreply.github.com>
2020-11-16 16:50:04 +0800

    Updates

---
+++ precache-manifest.4f7780c703f714c835e058a3ad4c99ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1964487490a527b41b16c3d554e72750",
+    "url": "/poke-graphql/index.html"
+  },
+  {
+    "revision": "3adcb22434a7ef1244cb",
+    "url": "/poke-graphql/static/css/main.c20dadcc.chunk.css"
+  },
+  {
+    "revision": "f4debc2fb144da428b36",
+    "url": "/poke-graphql/static/js/2.d7246c17.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/poke-graphql/static/js/2.d7246c17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3adcb22434a7ef1244cb",
+    "url": "/poke-graphql/static/js/main.6376abf1.chunk.js"
+  },
+  {
+    "revision": "7f519a4e0313d7668077",
+    "url": "/poke-graphql/static/js/runtime-main.e758fd45.js"
+  }
+]);
